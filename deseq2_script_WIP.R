45be4da458b5aafb77826e27b6c5e4f073d6a0db
Cassie <clettinger@ucdavis.edu>
2020-04-29 00:33:38 -0700

    Added taxonomy fixes & plotting functionality

--- deseq2_script_WIP.R
+++ deseq2_script_WIP.R
@@ -1,7 +1,12 @@
+#### WIP deseq2 script ####
+# Will merge when complete & running
+
 #DESEQ2 Analysis
 
 library(DESeq2)
 library(tidyverse)
+library(biobroom)
+library(patchwork)
 packageVersion("DESeq2") 
 
 
@@ -64,39 +69,124 @@ rm(missing, missing2, df.16s, df.ITS, i)
 
 ## ITS ##
 
+
+# Fix taxonomy so that there are no NA's & remove headers (e.g. p__)
+# This is important if we want to bind taxonomy to deseq results later
+
+df.ITS.tax <- data.frame(tax_table(ps.ITS.nocontrols))
+
+df.ITS.tax %<>% 
+  mutate(Phylum = fct_explicit_na(Phylum, na_level = "p__Unclassified"), 
+         Class = fct_explicit_na(Class, na_level = "c__Unclassified"), 
+         Order = fct_explicit_na(Order, na_level = "o__Unclassified"), 
+         Family = fct_explicit_na(Family, na_level = "f__Unclassified"), 
+         Genus = fct_explicit_na(Genus, na_level = "g__Unclassified"), 
+         Species = fct_explicit_na(Species, na_level = "s__Unclassified"))
+
+tax.list <- c("Phylum", "Class", "Order", "Family", "Genus", "Species")
+tax.header <- c(Phylum = "p__", Class = "c__", Order = "o__", 
+                Family = "f__", Genus = "g__", Species = "s__")
+
+for (i in tax.list) {
+  names <- sapply(strsplit(as.character(df.ITS.tax[[i]]), as.character(tax.header[[i]])), `[`, 2)
+  df.ITS.tax[[i]] <- names 
+  }
+
+row.names(df.ITS.tax) <- row.names(tax_table(ps.ITS.nocontrols))
+ITS.tax <- as.matrix(df.ITS.tax)
+
+tax_table(ps.ITS.nocontrols) <- ITS.tax
+
+
+## Background soil vs. all other treatments ##
 its.treat = phyloseq_to_deseq2(ps.ITS.nocontrols, ~ TreatmentName)
 
 dds.its.treat = DESeq(its.treat, test="Wald", fitType="parametric")
 
-contrasts = c("SA.G", "SA.ST", "SA.SAG", "SA.STG", 
-              "ST.G", "SAG.G", "STG.G", "ST.SAG",
-              "ST.STG", "SAG.STG", "SA.BSS", "ST.BSS",
+resultsNames(dds.its.treat) #gives us comparisons for our contrasts
+
+contrasts = c("SA.BSS", "ST.BSS",
               "G.BSS", "SAG.BSS", "STG.BSS")
 
 #I think I got them all?
-contrast.list <- list(SA.G = c("TreatmentName", "Stress_avoiding_forb", "Grass"),
-                      SA.ST = c("TreatmentName", "Stress_avoiding_forb", "Stress_tolerant_forb"),
-                      SA.SAG = c("TreatmentName", "Stress_avoiding_forb", "SA_forb_X_grass"),
-                      SA.STG = c("TreatmentName", "Stress_avoiding_forb", "ST_forb_X_grass"),
-                      ST.G = c("TreatmentName", "Stress_tolerant_forb", "Grass"),
-                      SAG.G = c("TreatmentName", "SA_forb_X_grass", "Grass"),
-                      STG.G = c("TreatmentName", "ST_forb_X_grass", "Grass"),
-                      ST.SAG = c("TreatmentName", "Stress_tolerant_forb", "SA_forb_X_grass"),
-                      ST.STG = c("TreatmentName", "Stress_tolerant_forb", "ST_forb_X_grass"),
-                      SAG.STG = c("TreatmentName", "SA_forb_X_grass", "ST_forb_X_grass"),
-                      SA.BSS = c("TreatmentName", "Stress_avoiding_forb", "Background_soil_and_sand_mix"),
+contrast.list <- list(SA.BSS = c("TreatmentName", "Stress_avoiding_forb", "Background_soil_and_sand_mix"),
                       ST.BSS = c("TreatmentName", "Stress_tolerant_forb", "Background_soil_and_sand_mix"),
-                      G.BSS = c("TreatmentName", "Grass", "Background_soil_and_sand_mix"),
+                      G.BSS = c("TreatmentName", "Invasive_grass", "Background_soil_and_sand_mix"),
                       SAG.BSS = c("TreatmentName", "SA_forb_X_grass", "Background_soil_and_sand_mix"),
                       STG.BSS = c("TreatmentName", "ST_forb_X_grass", "Background_soil_and_sand_mix"))
 
+
+plot.name.list <- list(SA.BSS = "SA forb vs. Background soil",
+                       ST.BSS = "ST forb vs. Background soil",
+                       G.BSS = "Invasive grass vs. Background soil",
+                       SAG.BSS = "SA forb X grass vs. Background soil",
+                       STG.BSS = "ST forb X grass vs. Background soil")
+
+alpha = 0.01
 res.list <- vector("list")
+plot.list = list()
 
 for(i in contrasts) {
-  res.list[[paste(r,i,sep = ".")]] <- broom::tidy(results(dds.its.treat, contrast = contrast.list[[i]], pAdjustMethod = "bonferroni"))
-}
+  #get results for each contrast
+  res <- results(dds.its.treat, contrast = contrast.list[[i]], pAdjustMethod = "bonferroni")
+  #filter results by p-value
+  res.alpha <- res[which(res$padj < alpha), ]
+  #Bind taxonomy to results
+  res.alpha.tax = cbind(as(res.alpha, "data.frame"), as(tax_table(ps.ITS.nocontrols)[rownames(res.alpha), ], "matrix"))
+  #tidy results 
+  res.list[[paste(i,sep = ".")]] <- tidy(res.alpha)
 
-#tidy results
-df <- plyr::ldply(res.list, function(x) x)
-names(df)[1] <- "Contrast"
-names(df)[2] <- "ASV"
+  #generate plot of significant ASVs for each contrast
+  # Order order
+  x = tapply(res.alpha.tax$log2FoldChange, res.alpha.tax$Order, function(x) max(x))
+  x = sort(x, TRUE)
+  res.alpha.tax$Order = factor(as.character(res.alpha.tax$Order), levels=names(x))
+  # Genus order
+  x = tapply(res.alpha.tax$log2FoldChange, res.alpha.tax$Genus, function(x) max(x))
+  x = sort(x, TRUE)
+  res.alpha.tax$Genus = factor(as.character(res.alpha.tax$Genus), levels=names(x))
+  p <- ggplot(res.alpha.tax, aes(x=Genus, y=log2FoldChange, color=Order)) + geom_point(size=6) + 
+    theme(axis.text.x = element_text(angle = -90, hjust = 0, vjust=0.5)) + ggtitle(plot.name.list[[i]])
+  plot_list[[i]] = p
+  }
+
+#plot results
+
+plot_list$SA.BSS + plot_list$ST.BSS + plot_list$G.BSS + plot_list$SAG.BSS + plot_list$STG.BSS
+
+#tidy results into a table to save
+df.res <- plyr::ldply(res.list, function(x) x)
+names(df.res)[1] <- "Contrast"
+names(df.res)[2] <- "ASV"
+
+write.csv(df.res, "its.ddseq.treatment.csv")
+
+
+
+
+
+# Now choosing a "new" control group to compare to
+#sample_data(ps.ITS.nocontrols)$TreatmentName <- relevel(sample_data(ps.ITS.nocontrols)$TreatmentName, "Grass")
+
+# All possible contrasts for TreamentName
+# contrasts = c("SA.G", "SA.ST", "SA.SAG", "SA.STG", 
+#               "ST.G", "SAG.G", "STG.G", "ST.SAG",
+#               "ST.STG", "SAG.STG", "SA.BSS", "ST.BSS",
+#               "G.BSS", "SAG.BSS", "STG.BSS")
+# 
+# 
+# contrast.list <- list(SA.G = c("TreatmentName", "Stress_avoiding_forb", "Invasive_grass"),
+#                       SA.ST = c("TreatmentName", "Stress_avoiding_forb", "Stress_tolerant_forb"),
+#                       SA.SAG = c("TreatmentName", "Stress_avoiding_forb", "SA_forb_X_grass"),
+#                       SA.STG = c("TreatmentName", "Stress_avoiding_forb", "ST_forb_X_grass"),
+#                       ST.G = c("TreatmentName", "Stress_tolerant_forb", "Invasive_grass"),
+#                       SAG.G = c("TreatmentName", "SA_forb_X_grass", "Invasive_grass"),
+#                       STG.G = c("TreatmentName", "ST_forb_X_grass", "Invasive_grass"),
+#                       ST.SAG = c("TreatmentName", "Stress_tolerant_forb", "SA_forb_X_grass"),
+#                       ST.STG = c("TreatmentName", "Stress_tolerant_forb", "ST_forb_X_grass"),
+#                       SAG.STG = c("TreatmentName", "SA_forb_X_grass", "ST_forb_X_grass"),
+#                       SA.BSS = c("TreatmentName", "Stress_avoiding_forb", "Background_soil_and_sand_mix"),
+#                       ST.BSS = c("TreatmentName", "Stress_tolerant_forb", "Background_soil_and_sand_mix"),
+#                       G.BSS = c("TreatmentName", "Invasive_grass", "Background_soil_and_sand_mix"),
+#                       SAG.BSS = c("TreatmentName", "SA_forb_X_grass", "Background_soil_and_sand_mix"),
+#                       STG.BSS = c("TreatmentName", "ST_forb_X_grass", "Background_soil_and_sand_mix"))
