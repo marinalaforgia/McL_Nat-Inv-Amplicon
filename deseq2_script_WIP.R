58e9362c60a3294b01e7985c2879fd2436d1c381
Cassie <clettinger@ucdavis.edu>
2020-04-28 22:03:33 -0700

    Fixed contrast

--- deseq2_script_WIP.R
+++ deseq2_script_WIP.R
@@ -1,6 +1,9 @@
 #DESEQ2 Analysis
 
 library(DESeq2)
+library(tidyverse)
+packageVersion("DESeq2") 
+
 
 ### Read in Data ###
 
@@ -18,7 +21,7 @@ ps.ITS.nocontrols <- readRDS("Data/greenhouse_its_itsx_decontam_controlsremoved.
 
 # Metadata mapping file including biomass and traits (version4)
 mapping <- read.csv("/Users/Marina/Documents/UC-Davis/Projects/McL_Nat-Inv-Amplicon/Data/Setup/Grassland-Amplicon-Mapping-File4.csv") 
-mapping <- read.csv("/Data/Grassland-Amplicon-Mapping-File4.csv") #Cassie's path
+mapping <- read.csv("Data/Grassland-Amplicon-Mapping-File4.csv") #Cassie's path
 
 #Adding columns to the mapping file
 mapping$FunGroup <- ifelse(mapping$TreatmentName == "Invasive_grass", "Grass", NA)
@@ -46,3 +49,54 @@ sample_data(ps.ITS.nocontrols) # fixed!
 sample_data(ps.16s.nocontrols) <- mapping
 
 rm(missing, missing2, df.16s, df.ITS, i)
+
+
+#### Differential abundance analysis ####
+
+# Write scripts to test for ASVs that differ with:
+# Plant species
+# Competition
+# These should mirror ordination comparisons, etc
+
+
+# Can be done at ASV level or at higher levels of taxonomic classification (e.g. family, order)
+# Starting with only the ASV level
+
+## ITS ##
+
+its.treat = phyloseq_to_deseq2(ps.ITS.nocontrols, ~ TreatmentName)
+
+dds.its.treat = DESeq(its.treat, test="Wald", fitType="parametric")
+
+contrasts = c("SA.G", "SA.ST", "SA.SAG", "SA.STG", 
+              "ST.G", "SAG.G", "STG.G", "ST.SAG",
+              "ST.STG", "SAG.STG", "SA.BSS", "ST.BSS",
+              "G.BSS", "SAG.BSS", "STG.BSS")
+
+#I think I got them all?
+contrast.list <- list(SA.G = c("TreatmentName", "Stress_avoiding_forb", "Grass"),
+                      SA.ST = c("TreatmentName", "Stress_avoiding_forb", "Stress_tolerant_forb"),
+                      SA.SAG = c("TreatmentName", "Stress_avoiding_forb", "SA_forb_X_grass"),
+                      SA.STG = c("TreatmentName", "Stress_avoiding_forb", "ST_forb_X_grass"),
+                      ST.G = c("TreatmentName", "Stress_tolerant_forb", "Grass"),
+                      SAG.G = c("TreatmentName", "SA_forb_X_grass", "Grass"),
+                      STG.G = c("TreatmentName", "ST_forb_X_grass", "Grass"),
+                      ST.SAG = c("TreatmentName", "Stress_tolerant_forb", "SA_forb_X_grass"),
+                      ST.STG = c("TreatmentName", "Stress_tolerant_forb", "ST_forb_X_grass"),
+                      SAG.STG = c("TreatmentName", "SA_forb_X_grass", "ST_forb_X_grass"),
+                      SA.BSS = c("TreatmentName", "Stress_avoiding_forb", "Background_soil_and_sand_mix"),
+                      ST.BSS = c("TreatmentName", "Stress_tolerant_forb", "Background_soil_and_sand_mix"),
+                      G.BSS = c("TreatmentName", "Grass", "Background_soil_and_sand_mix"),
+                      SAG.BSS = c("TreatmentName", "SA_forb_X_grass", "Background_soil_and_sand_mix"),
+                      STG.BSS = c("TreatmentName", "ST_forb_X_grass", "Background_soil_and_sand_mix"))
+
+res.list <- vector("list")
+
+for(i in contrasts) {
+  res.list[[paste(r,i,sep = ".")]] <- broom::tidy(results(dds.its.treat, contrast = contrast.list[[i]], pAdjustMethod = "bonferroni"))
+}
+
+#tidy results
+df <- plyr::ldply(res.list, function(x) x)
+names(df)[1] <- "Contrast"
+names(df)[2] <- "ASV"
